47ee7684dff8dd05daeeb73bfab6e5c11dbe6b2f
Praveen J <75038640+praveencash@users.noreply.github.com>
2025-04-29 02:42:00 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,8 @@
+from utils.greetings import say_hello
+from utils.maths import add, multiply
+from utils.texttools import count_chars
+
+say_hello("Praveen")
+print("5 + 3 =", add(5, 3))
+print("5 × 3 =", multiply(5, 3))
+print("Characters in 'hello world':", count_chars("hello world"))
